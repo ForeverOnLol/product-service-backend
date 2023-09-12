f17804f82d8c1cde987c793c3f603ddc7b59ddb4
foreveronlol <koval_fol@yahoo.com>
2023-09-12 19:04:14 +0300

    Тестовое API для проверки докера

--- service/products/views.py
+++ service/products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class TestDataApiView(APIView):
+    def get(self, request):
+        # Получаем набор всех записей из таблицы Capital
+        # Сериализуем извлечённый набор записей
+        data = {'test': 'data', 'hello': 'world'}
+        return Response(data)
